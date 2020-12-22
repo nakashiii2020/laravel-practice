cbba711cb1daffece8a5a8e1713586950704fcff
nakashiii <nakashima.k.1991@gmail.com>
2020-12-23 07:40:27 +0900

    add delete (turtorial done)

--- src/routes/web.php
+++ src/routes/web.php
@@ -51,5 +51,6 @@ Route::post('/task', function (Request $request) {
  * タスク削除
  */
 Route::delete('/task/{task}', function (Task $task) {
-    //
+    $task->delete();
+    return redirect('/');
 });
